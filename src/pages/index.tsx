e19b6ad7ad90fe25122fe4212a3a2cae80b39751
wilk.gillian <wilk.silva@mt.senac.br>
2022-12-19 12:31:00 -0300

    ..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import { GetStaticProps } from 'next';
-
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Header from '../components/Header';
+import { GetStaticProps } from 'next';
+import {FiCalendar, FiUser} from 'react-icons/fi'
+import Link from 'next/link';
 
 interface Post {
   uid?: string;
@@ -24,13 +26,50 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <main className={commonStyles.container}>
+      <Header />
+      <div>
+        {postsPagination.results.map(post => {
+          <Link passHref href={`/post/${post.uid}`}>
+            <a className={styles.post}>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  {post.first_publication_date}</li>
+                <li>
+                  <FiUser />
+                {post.data.author}</li>
+              </ul>
+            </a>
+          </Link>
+        })}
+      </div>
+    </main>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', {
+    pageSize: 5,
+    orderings: {
+      field: 'last_publication_date',
+      direction: 'desc',
+    },
+  });
 
-//   // TODO
-// };
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
